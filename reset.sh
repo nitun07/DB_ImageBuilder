020c4ace12159f2933829ca8ac7380148f2560cf
Nitun Das <nitun_2007@rediffmail.com>
2020-04-30 22:19:18 +0530

    minor script changes

--- reset.sh
+++ reset.sh
@@ -1,24 +1,16 @@
 #!/bin/bash -e
 
-IMAGE_ARCH=$1
-DISTRO=$2
 
-echo "Reset Open.HD Builder"
+echo "Reset Image Builder"
 echo "------------------------------------------------------"
 echo ""
 
+echo "Usage: ./reset.sh"
+echo "------------------------------------------------------"
+echo ""
 
-if [[ "${IMAGE_ARCH}" == "" || ${DISTRO} == "" ]]; then
-    IMAGE_ARCH="pi"
-    DISTRO="stretch"
-
-    echo "Usage: ./reset.sh pi [stretch | buster]"
-    echo "------------------------------------------------------"
-    echo ""
-fi
-
-if [ -d "work-${IMAGE_ARCH}-${DISTRO}" ]; then
-    pushd "work-${IMAGE_ARCH}-${DISTRO}"
+if [ -d "work-directory" ]; then
+    pushd "work-directory"
         find . -type f -name 'SKIP' -delete
         find . -type f -name 'SKIP_STEP*' -delete
         find . -type f -name 'SKIP_CH_STEP*' -delete
